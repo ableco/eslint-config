f6c7530acade67ba2c83501c801824e10eeaf094
Julio García <julioggonz@gmail.com>
2019-10-17 16:20:53 -0500

    Remove eslint extends

--- index.js
+++ index.js
@@ -6,39 +6,41 @@ module.exports = {
     jest: true
   },
   parser: "babel-eslint",
-  extends: "react-app",
-  plugins: ["import", "react-hooks", "prettier"],
+  plugins: ["import", "react", "react-hooks", "prettier"],
   rules: {
-    "no-dupe-keys": "warn",
-    "no-undef": "warn",
-    "no-unreachable": "warn",
+    "no-dupe-keys": "error",
+    "no-undef": "error",
+    "no-unreachable": "error",
     "no-unused-vars": [
-      "warn",
+      "error",
       {
         argsIgnorePattern: "^_",
         varsIgnorePattern: "^_",
         ignoreRestSiblings: true
       }
     ],
-    "no-useless-constructor": "warn",
-    "no-var": "warn",
-    "no-duplicate-imports": "warn",
-    "no-duplicate-case": "warn",
-    "import/no-unresolved": "warn",
-    "import/default": "warn",
-    "react/jsx-no-undef": "warn",
-    "react/jsx-uses-vars": "warn",
-    "react/jsx-uses-react": "warn",
-    "react/react-in-jsx-scope": "warn",
-    "react/no-string-refs": "warn",
-    "react/prop-types": ["warn", { skipUndeclared: true }],
-    "react/forbid-prop-types": "warn",
-    "react/prefer-stateless-function": ["warn", { ignorePureComponents: true }],
-    "react-hooks/rules-of-hooks": "warn", // Checks rules of Hooks
-    "react-hooks/exhaustive-deps": "warn", // Checks effect dependencies
-    "prettier/prettier": "warn",
-    "no-console": ["warn", { allow: ["warn", "error"] }],
-    "no-debugger": "warn"
+    "no-useless-constructor": "error",
+    "no-var": "error",
+    "no-duplicate-imports": "error",
+    "no-duplicate-case": "error",
+    "import/no-unresolved": "error",
+    "import/default": "error",
+    "react/jsx-no-undef": "error",
+    "react/jsx-uses-vars": "error",
+    "react/jsx-uses-react": "error",
+    "react/react-in-jsx-scope": "error",
+    "react/no-string-refs": "error",
+    "react/prop-types": ["error", { skipUndeclared: true }],
+    "react/forbid-prop-types": "error",
+    "react/prefer-stateless-function": [
+      "error",
+      { ignorePureComponents: true }
+    ],
+    "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
+    "react-hooks/exhaustive-deps": "error", // Checks effect dependencies
+    "prettier/prettier": "error",
+    "no-console": ["error", { allow: ["warn", "error"] }],
+    "no-debugger": "error"
   }
 };
 
